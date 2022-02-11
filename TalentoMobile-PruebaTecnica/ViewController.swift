5f904c88e1f23376c16edf9c324eff09a68be390
David Alba <david.alba@gft.com>
2022-02-11 11:19:50 +0100

    Initial commit

---
+++ TalentoMobile-PruebaTecnica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TalentoMobile-PruebaTecnica
+//
+//  Created by Alba Domingo, David on 11/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
